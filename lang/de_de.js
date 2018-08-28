96e3a2447af56b000a08073a68e657616b4ad7df
Jack Dexter <rmatschke@uos.de>
2018-08-28 10:41:11 -0300

    third commit

--- lang/de_de.js
+++ lang/de_de.js
@@ -1,86 +1,82 @@
 class T {
 	constructor() {
-		this.page_title = "Mahlowat";
-		this.qa_modal_title = "Fragen &amp; Antworten";
-		this.qa_modal_body = '<h4>Wer steht hinter dem Mahlowat?</h4>\
-					<p>Lorem Ipsum.</p>\
+		this.page_title = "O Votômetro";
+		this.qa_modal_title = "FAQ";
+		this.qa_modal_body = '<h4></h4>\
+					<p></p>\
 \
-					<h4>Wer hat die Thesen erarbeitet?</h4>\
-					<p>Lorem Ipsum.</p>\
+					<h4>Quem somos nós?</h4>\
+					<p>Somos um grupo de amigos que pretende fornecer uma ferramenta complementar para te ajudar a conhecer os candidatos e decidir o seu voto.</p>\
 \
-					<h4>Woher stammen die Positionen der Gruppen?</h4>\
-					<p>Den an der Wahl teilnehmenden Gruppen wurden die Thesen mit der Bitte um Stellungnahme zugeleitet. Neben der reinen\
-						Positionierung (Zustimmung/Neutral/Ablehnung/Keine Stellungnahme) konnten sie ihre Position auch in einem kurzen Absatz\
-						erläutern.\
+					<h4>Como pretendemos fazer isso?</h4>\
+					<p>Selecionamos alguns pontos que consideramos fundamentais na agenda política desse ano. Com eles, você poderá ver o quanto os candidatos pensam como você. Destacamos que as perguntas aqui analisadas não pretendem cobrir todos os assuntos importantes a respeito das eleições, e acreditamos que mais pesquisas sobre os candidatos são fundamentais.\
 					</p>\
-					<p>Für ihre Stellungnahmen zu den Thesen sind die Gruppen selbst verantwortlich.</p>\
+					<p></p>\
 \
-					<h4>Von welcher Wahl reden wir hier überhaupt?</h4>\
-					<p>Lorem Ipsum.</p>\
+					<h4>Onde foram obtidas as informações sobre os candidatos?</h4>\
+					<p>As informações sobre as posições dos candidatos foram retiradas de seus programas, entrevistas dos próprios candidatos divulgadas na mídia e informações sobre as votações do partido do candidato em votações no Congresso Nacional. Além disso, este Quiz foi baseado em outra iniciativa semelhante, o https://oiceberg.com.br.</p>\
 \
-					<h4>Wie werden die Punkte berechnet?</h4>\
-					<p>Deine Antworten werden mit den vorgegebenen Antworten der Gruppen abgeglichen.</p>\
+					<h4>Como os pontos são calculados?</h4>\
+					<p> Suas respostas serão comparadas com as posições dos presidenciáveis.</p>\
 					<ul>\
-						<li>Stimmt die Antwort überein, werden der Gruppe 2 Punkte gutgeschrieben;</li>\
-						<li>Weicht die Antwort leicht ab (Zustimmung/Neutral oder Neutral/Ablehnung), wird der Gruppe 1 Punkt gutgeschrieben;</li>\
-						<li>Sind die Antworten entgegengesetzt oder hat eine Gruppe eine These nicht beantwortet, gibt es keine Punkte für die\
-							Gruppe.\
+						<li>    Se sua resposta for a mesma do candidato, ele ganha 2 pontos;</li>\
+						<li>Se a resposta for diferente, mas não contrária (no caso da posição de um ser "neutra" e de outro "aprovação" ou "rejeição"), é um ponto para o candidato;</li>\
+						<li>Se as respostas forem opostas, não há pontos. As questões que você pular não serão pontuadas.\
 						</li>\
 					</ul>\
-					<p>Eine These, die du übersprungen hast, wird nicht gewertet. Die erreichbare Höchstpunktzahl wird dadurch geringer.</p>\
-					<p>Eine These, die doppelt gewichtet werden soll, wird doppelt gewichtet, das heißt, für sie wird die doppelte Punktzahl\
-						gutgeschrieben (0/2/4). Dadurch können insgesamt mehr Punkte erreicht werden.</p>\
+					<p>Uma pergunta em que você selecionar dobrar importância é ponderada duas vezes, garantindo uma pontuação dupla (seja 0, 2 ou 4, dependendo da sua posição e do candidato).</p>\
+					<p></p>\
 \
-					<h4>Werden meine Antworten gespeichert?</h4>\
-					<p>Nein. Alles läuft vollständig in deinem Browser ab.</p>\
+					<h4>Minhas respostas serão salvas?</h4>\
+					<p>Não, nada do que você responder será salvo.</p>\
 \
-					<h4>Ich habe einen inhaltlichen Fehler gefunden!</h4>\
-					<p>Gib uns gern Bescheid, wir sehen uns das an. Wer „wir“ sind, steht oben auf dieser Seite.</p>\
+					<h4>Eu encontrei um erro de conteúdo!</h4>\
+					<p>Por favor, nos envie um e-mail para gussabbag@gmail.com.</p>\
 \
-					<h4>Wer hat den Mahlowat programmiert?</h4>\
-					<p>Das steht\
-						<a href="https://github.com/hszemi/mahlowat">hier</a>. Der Mahlowat ist übrigens freie Software!</p>\
+					<h4>Quem programou o Votômetro?</h4>\
+					<p>Está \
+						<a href="https://github.com/hszemi/mahlowat">aqui</a>.</p>\
 \
-					<h4>Ich habe einen Programmierfehler gefunden!</h4>\
-					<p>Oh nein! Wenn du den Fehler\
-						<a href="https://github.com/hszemi/mahlowat">meldest</a>, wird er vielleicht behoben.</p>';
-		this.btn_qa_modal_close = "Schließen";
-		this.swype_info_message_text = "Wische, um manuell zwischen Thesen zu wechseln";
-		this.btn_swype_info_ok = "OK";
+					<h4>Eu encontrei um erro de programação!</h4>\
+					<p>Oh não! Se você reportar o erro, ele pode ser corrigido.\
+						<a href="https://github.com/hszemi/mahlowat"></a></p>';
+		this.btn_qa_modal_close = "Fechar";
+		this.swype_info_message_text = "Deslize para alternar manualmente entre as teses";
+		this.btn_swype_info_ok = "Ta Bom";
 
-		this.start_subtitle = "Der Mahlowat ist ein Wahlpositions&shy;vergleichswerkzeug.";
-		this.start_explanatory_text = "<p>Der Mahlowat ermöglicht es dir, deine Meinung zu ausgewählten Thesen mit denen der Gruppen zu vergleichen, die zur $WAHL\
-						antreten.\
+		this.start_subtitle = "O Votômetro é uma ferramenta para identificar a correspondência entre suas ideias e as dos candidatos.";
+		this.start_explanatory_text = "<p> O Votômetro compara suas posições quanto a assuntos importantes da agenda política com as opiniões dos presidenciáveis de 2018.\
 					</p>\
-					<p>Für ihre Stellungnahmen zu den Thesen sind die Gruppen selbst verantwortlich.</p>";
-		this.btn_start = "Mahlowat starten!";
-		this.btn_start_show_qa = "Fragen &amp; Antworten";
-		this.btn_toggle_thesis_more_text = "Erläuterung";
-		this.btn_important = "These doppelt gewichten";
-		this.btn_yes_text = "Zustimmung";
-		this.btn_neutral_text = "Neutral";
-		this.btn_no_text = "Ablehnung";
-		this.btn_skip_text = "Überspringen";
-		this.btn_mahlowat_show_start = "Zurück zur Startseite";
-		this.btn_mahlowat_show_qa = "Fragen &amp; Antworten";
-		this.btn_mahlowat_skip_remaining_theses = "Alle verbleibenden Thesen überspringen und aktuellen Stand auswerten";
-		this.title_results = "Ergebnis";
-		this.title_results_summary = "Zusammenfassung";
-		this.text_result_below_summary = '<small>Nicht zufrieden mit dem Ergebnis?\
-				<button class="btn btn-sm btn-light" onclick="showMahlowatFirstThesis()">Ändere die Antworten oder die Gewichtung!</button>\
+					<p></p>";
+		this.btn_start = "começar";
+		this.btn_start_show_qa = "FAQ";
+		this.btn_toggle_thesis_more_text = "O que é Dobrar Importância?";
+		this.btn_important = "contar o dobro";
+		this.btn_yes_text = "concordo";
+		this.btn_neutral_text = "neutro";
+		this.btn_no_text = "discordo";
+		this.btn_skip_text = "ignore";
+		this.btn_mahlowat_show_start = "voltar para a home page";
+		this.btn_mahlowat_show_qa = "FAQ";
+		this.btn_mahlowat_skip_remaining_theses = "Ignore todas as teses restantes e avalie o status atual";
+		this.title_results = "";
+		this.title_results_summary = "Resultados";
+		this.text_result_below_summary = '<small>Não satisfeito com o resultado?\
+				<button class="btn btn-sm btn-light" onclick="showMahlowatFirstThesis()">Altere as respostas ou a ponderação!</button>\
 			</small>';
 		this.title_results_details = "";
-		this.btn_results_show_start = "Zurück zur Startseite";
-		this.btn_results_show_qa = "Fragen &amp; Antworten";
+		this.btn_results_show_start = "voltar para a home page";
+		this.btn_results_show_qa = "FAQ";
 
 	}
 
-	thesis_number(number) { return "These " + number; }
-	get btn_make_thesis_double_weight() { return "These doppelt gewichten"; }
-	get btn_thesis_has_double_weight() { return "These wird doppelt gewichtet"; }
-	get label_your_choice() { return "Deine Wahl"; }
-	achieved_points_text(pointsForList, maxAchievablePoints) { return '' + pointsForList + '/' + maxAchievablePoints + ' Punkte'; }
-	get default_text_no_statement() { return "<small class='text-muted'>Keine Stellungnahme.</small>"; }
+
+	thesis_number(number) { return "" ; }
+	get btn_make_thesis_double_weight() { return "Clique aqui para Dobrar Importância"; }
+	get btn_thesis_has_double_weight() { return "Tese contada em dobro"; }
+	get label_your_choice() { return "Sua escolha"; }
+	achieved_points_text(pointsForList, maxAchievablePoints) { return '' + Math.round((pointsForList/maxAchievablePoints)*100) + '%'; }
+	get default_text_no_statement() { return "<small class='text-muted'></small>"; }
 	get error_loading_config_file() {
 		return '<b>Fehler</b> Die Konfigurationsdatei <a href="config/data.json"><tt>config/data.json</tt></a> konnte nicht geladen\
 		werden. Existiert sie und enthält keine Syntaxfehler?';
